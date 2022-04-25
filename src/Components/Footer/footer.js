724f10fe1052f1874ea1d5e008338bcd18b5b993
Abasiakara_Alexander <abasiakaraalexander@gmail.com>
2022-04-25 17:18:52 +0100

    Navbar and Footer

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -7,15 +7,15 @@ const footer = () => {
         <div className="footer-flex">
           <div className="ff1">
             <h2>Address:</h2>
-            <p>Kilimanjaro Retaurant</p>
+            <p>SH Retaurant</p>
             <p>153 Ikot Ekpene road, Uyo</p>
           </div>
           <div className="ff2">
             <h2>Reservation:</h2>
             <p>08012345678</p>
-            <p>kilimanjaro@gmail.com</p>
+            <p>shrestaurant@gmail.com</p>
           </div>
-          <div>
+          <div className="ff3">
             <h2>Open Hours:</h2>
             <p>Monday - Friday: 10AM - 11PM</p>
             <p>Saturday - Sunday: 9AM - 1PM</p>
@@ -29,10 +29,10 @@ const footer = () => {
           <a href="#">
             <span class="iconify" data-icon="brandico:twitter-bird"></span>
           </a>
-          <a href="mailto:kilifoods@gmail.com">
+          <a href="mailto:shrestaurant@gmail.com">
             <span class="iconify" data-icon="ci:mail"></span>
           </a>
-          <p>copyright @2022 Kilimanjaro Restaurant</p>
+          <p>copyright @2022 SHRestaurant</p>
         </div>
       </footer>
     </div>
