1a5da06ceed88d51c2a23f8d117cb45652a0c978
Alpha-creed <98430379+Alpha-creed@users.noreply.github.com>
2022-04-27 15:06:34 +0000

    Add files via upload

---
+++ Updatedproduct/Product.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FoodProduct from './components/FoodProduct'
+import SampleCover from './components/SampleCover'
+
+export default function Product() {
+  return (
+    <div>
+      <SampleCover/>
+      <FoodProduct/>
+    </div>
+  )
+}
---
+++ Updatedproduct/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Product from './Product';
+import './resturant.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Product/>
+  </React.StrictMode>
+);
+
